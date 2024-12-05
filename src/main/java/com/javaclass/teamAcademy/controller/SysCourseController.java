a2ccc4e4b70d7ff4ba39347794797d297981088e
2m1nj1 <minjijic@gmail.com>
2024-12-05 16:20:50 +0900

    2024-12-05 PM04:20 / 이민지 - Controller 복구

--- src/main/java/com/javaclass/teamAcademy/controller/SysCourseController.java
+++ src/main/java/com/javaclass/teamAcademy/controller/SysCourseController.java
@@ -1,7 +1,6 @@
 package com.javaclass.teamAcademy.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
